b169789e24dc04b8da0ed842cd204dc998bd6cd3
Botond Magyarosi <mboti@halcyonmobile.com>
2018-09-18 13:21:28 +0300

    Create simple release script for CocoaPods releases

--- release.sh
+++ release.sh
@@ -6,20 +6,22 @@ echo "Are you sure you meant to run this script?"
 echo "This script releases a new version of ReleaseRadar!"
 echo ""
 
-while [ $wait_time >= 1 ]
+while [[ $wait_time -ge 1 ]]
 do
     printf "\rYou have %2d second(s) remaining to hit Ctrl+C to cancel that operation!" $wait_time
     sleep 1
-    temp_cnt=$temp_cnt-1
+    wait_time=$((wait_time-1))
 done
 
+echo ""
 echo ""
 echo "🚀  Releasing ..."
 echo ""
 
 pod lib lint
 
-git add -A && git commit -m "Release 0.1.0"
-git tag '0.1.0'
-git push --tags
+# git add -A && git commit -m "Release 0.1.0"
+# git tag '0.1.0'
+# git push --tags
 
+pod trunk push ReleaseRadar.podspec
